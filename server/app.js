6a89d85d51c2558e8b621bf0cdbbb8fc8c581edf
Michael Dillon <michaelcdillon@me.com>
2012-01-31 15:56:40 -0500

    fixed post from get for github hooks

--- server/app.js
+++ server/app.js
@@ -35,7 +35,7 @@ app.configure('production', function(){
 // Routes
 
 app.get ('/', routes.index);
-app.get ('/newPost', routes.newPost);
+app.post ('/newPost', routes.newPost);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
