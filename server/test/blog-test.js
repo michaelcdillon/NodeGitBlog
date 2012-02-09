47a85e4dc8cd6278c6990ab8b53a367fbe74cee6
Michael Dillon <michaelcdillon@me.com>
2012-02-08 23:02:18 -0500

    successfully added nodemock support and implemented it in blog-test.js

--- server/test/blog-test.js
+++ server/test/blog-test.js
@@ -3,7 +3,9 @@ process.env.NODE_ENV = 'test';
 var testCase = require ('nodeunit').testCase;
 
 var blog = require ('../lib/blog');
+var nodemock = require ('nodemock');
 
+var mockedDao;
 
 exports.groupParseMetaDataAndContent = testCase ({
     setUp: function (next) {
@@ -37,12 +39,47 @@ exports.groupParseMetaDataAndContent = testCase ({
 
 exports.groupPreparePost = testCase ({
     setUp: function (next) {
+        mockedDao = nodemock.mock ("fetchPostForDisplay")
+                            .takes ("Test Title", function (){})
+                            .calls (1, [{
+                                email : "email",
+                                username : "username",
+                                name : "name",
+                            }, {
+                                title : "title",
+                            }, {
+                                commitId : "commitId",
+                                revisionDate : "revisionDate",
+                                content : "content"
+                            }]);
+
+        blog.setup (mockedDao);
         next ();
     },
 
     tearDown: function (next) {
+        blog.setup (null);
         next ();
     },
+    
+    preparePostTest: function (test) {
+        var req = {
+            params : {
+                title : "Test Title",
+            },
+        };
+        var res;
 
+        blog.preparePost (req, res, function () {
+            test.equal (req.postEmail, "email");
+            test.equal (req.postUsername, "username");
+            test.equal (req.postName, "name");
+            test.equal (req.postCommitId, "commitId");
+            test.equal (req.postDate, "revisionDate");
+            test.equal (req.postTitle, "title");
+            test.equal (req.postContent, "content");     
+            test.done ();
+        });
+    },
     
 });
